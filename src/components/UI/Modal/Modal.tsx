0b5d46276af1bb425782b5094fa40a5e09ae99b5
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-23 14:42:31 +0600

    modal & loader added

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,33 @@
+import { Modal, Box } from '@mui/material';
+import { FC, PropsWithChildren } from 'react';
+
+interface Props {
+  open: boolean;
+}
+
+const UIModal: FC<PropsWithChildren<Props>> = ({ open, children }) => {
+  return (
+    <Modal open={open}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          position: 'absolute',
+          top: 100,
+          bottom: 100,
+          left: '50%',
+          transform: 'translate(-50%)',
+          width: 600,
+          bgcolor: 'background.paper',
+          boxShadow: 24,
+          px: 4,
+          py: 8,
+        }}
+      >
+        {children}
+      </Box>
+    </Modal>
+  );
+};
+
+export default UIModal;
